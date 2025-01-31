443ebc1bd4fc44e6f9ccea9817394ec41c809642
soumaya <soumaya.rabah68@uha.fr>
2025-01-31 22:35:09 +0100

    MAJ

--- Analyse_Gros_fichiers.ps1
+++ Analyse_Gros_fichiers.ps1
@@ -1,9 +1,9 @@
 # Analyse_Gros_fichiers.ps1
 
-# Chemin vers ton fichier Python (mettez ici le chemin complet de ton fichier Python)
+# Chemin vers fichier Python
 $chemin_python = "/Users/soumaya/PycharmProjects/SAE-Traiter-des-donn-s/analyse_fichiers.py"
 
-# Répertoire de base à analyser (remplace par ton répertoire d'analyse)
+# Répertoire de base à analyser
 $repertoire_de_base = "/Users/soumaya/Documents/BUT 1ère année"
 
 # Exécute le script Python avec le répertoire de base comme argument
--- analyse_fichiers.py
+++ analyse_fichiers.py
@@ -43,3 +43,4 @@ def main():
 # Exécution du script
 if __name__ == "__main__":
     main()
+
