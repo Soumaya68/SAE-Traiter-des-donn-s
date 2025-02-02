66b83772bb99829910dd7058d093e62a510ff337
soumaya <soumaya.rabah68@uha.fr>
2025-02-02 20:59:12 +0100

    MAJ

---
+++ Powershell.ps1
@@ -0,0 +1,6 @@
+# Chemin vers le fichier Python
+$chemin_python = "/Users/soumaya/PycharmProjects/SAE-Traiter-des-donn-s/main.py"
+
+# Exécuter le script Python avec les arguments
+python3 "$chemin_python"
+
